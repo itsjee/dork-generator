c699a586a61c6cc38fc3ecc38505b4903b4247a6
Caner <2broglie@gmail.com>
2020-12-05 01:42:21 +0100

    Beginning of the project

---
+++ dork-generator.py
@@ -0,0 +1,12 @@
+class RawQuery(object):
+
+	def __init__(self):
+		self.title = ""
+		self.url = ""
+		self.site = ""
+		self.text = ""
+		self.extension = ""
+		self.filetype = ""
+		self.allintitle = ""
+		self.allinurl = ""
+		self.related = ""
